9e007c33b7e6888647d1c9ba8c7ec3cc6b141b26
Dhrub <burhd2707@gmail.com>
2023-12-19 17:03:13 +0530

    IncompleteProject

---
+++ ContextApiProject2/src/App.jsx
@@ -0,0 +1,26 @@
+// import { useState } from 'react'
+import './App.css'
+import { ThemeProvider } from './Context/Theme'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <ThemeProvider>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
+
+  )
+}
+
+export default App
